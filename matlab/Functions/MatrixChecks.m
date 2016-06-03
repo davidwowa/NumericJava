917ba31c1987709f0e14b21e75fe5b5feb5a30c2
Wladimir David Zakrevskyy <wd.zakrevskyy@icloud.com>
2016-06-04 00:11:08 +0200

    - preparation last exercise

--- matlab/Functions/MatrixChecks.m
+++ matlab/Functions/MatrixChecks.m
@@ -6,6 +6,22 @@ classdef MatrixChecks
     end
     
     methods
+        function x=backSubstitution(obj,b,n)
+            % Solving an upper triangular system by back-substitution
+            % Input matrix U is an n by n upper triangular matrix
+            % Input vector b is n by 1
+            % Input scalar n specifies the dimensions of the arrays
+            % Output vector x is the solution to the linear system
+            % U x = b
+            % K. Ming Leung, 01/26/03
+            
+            x=zeros(n,1);
+            for j=n:-1:1
+                if (obj.A(j,j)==0); error('Matrix is singular!'); end;
+                x(j)=b(j)/obj.A(j,j);
+                b(1:j-1)=b(1:j-1)-obj.A(1:j-1,j)*x(j);
+            end
+        end
         function result = isPositiveDefinit(obj)
             disp('start calcualte eigenvalues')
             tic
