0908428403b625351997e8d493da17b16a7079ad
Wladimir David Zakrevskyy <wd.zakrevskyy@icloud.com>
2016-06-05 02:15:07 +0200

    - jama for infinity norms - matlab things - exercise one is ready

--- matlab/Functions/GraphLogScale.m
+++ matlab/Functions/GraphLogScale.m
@@ -1,27 +1,41 @@
 classdef GraphLogScale
     properties
-    x
-    y
-    p
+        x
+        y
+        p
+
+        y1
+        p1
+
+        y2
+        p2
+
     end
     
     methods
-        function g = log_plot_X(obj)
+        function g = log_plot_X(obj, labelName, xlabelT, ylabelT)
             figure
-            g = semilogx(obj.x, obj.y);
+            g = semilogx(obj.x, obj.y, 'LineWidth',2);
+            xlabel(xlabelT);
+            ylabel(ylabelT);
+            legend(labelName, 'Location','southoutside');
         end
         
-        function g = log_plot_Y(obj)
+        function log_plot_Y(obj, labelName, xlabelT, ylabelT)
             figure
-            g = semilogy(obj.x, obj.y);
+            semilogy(obj.x, obj.y, obj.x, obj.y1, obj.x, obj.y2, 'LineWidth',2);
+            xlabel(xlabelT);
+            ylabel(ylabelT);
+            legend(labelName, 'Cholesky (Java)', 'LU (Java)', 'Location','southoutside');
         end
-        
+
+
         function g = scater_plot(obj)
             g = scatter(obj.x, obj.y);
         end
         
         function g = f_plot(obj, from, to)
-            g = fplot(obj.p, [from, to], 'LineWidth',2); 
+            g = fplot(obj.p, [from, to], 'LineWidth',2);
         end
     end
 end
--- src/de/wdz/numeric/apache/lu/A_LU.java
+++ src/de/wdz/numeric/apache/lu/A_LU.java
@@ -4,7 +4,9 @@ import org.apache.commons.math3.linear.Array2DRowRealMatrix;
 import org.apache.commons.math3.linear.BlockRealMatrix;
 import org.apache.commons.math3.linear.LUDecomposition;
 import org.apache.commons.math3.linear.RealMatrix;
+import org.apache.commons.math3.linear.SingularValueDecomposition;
 
+import Jama.Matrix;
 import de.wdz.numeric.matrix.MatrixGenerator;
 
 public class A_LU {
@@ -12,8 +14,10 @@ public class A_LU {
 	public static void main(String[] args) {
 		MatrixGenerator generator = new MatrixGenerator();
 
-		System.out.println("n\t\t\t\tresNorm\t\t\t\t\t\terrNorm");
-		for (int n = 2; n < 12; n++) {
+		// positive definite matrix must be
+
+		System.out.println("n\tr\t\t\t\t\t\t\t" + "∆x" + "\t\t\t\t\t\t\tcondition\t\t\t\t\t\t\tequal numbers");
+		for (int n = 2; n < 10; n++) {
 
 			double[][] xOnes = generator.getVectorWithOnes(n);
 			double[][] hilbertMatrix = generator.getHilbertMatrix(n);
@@ -21,24 +25,50 @@ public class A_LU {
 			RealMatrix H = new BlockRealMatrix(hilbertMatrix);
 			RealMatrix x = new Array2DRowRealMatrix(xOnes);
 
+			double conitionNumber = new SingularValueDecomposition(H).getConditionNumber();
+
 			RealMatrix b = H.multiply(x);
 
 			LUDecomposition decomposition = new LUDecomposition(H);
 
+			// Solution
 			RealMatrix x_hut = decomposition.getSolver().solve(b);
 
+			// System.out.println(x_hut.toString());
+
 			// Residuum
-			// RealMatrix residuum = b.subtract(H);
+			RealMatrix residuum = H.multiply(x_hut).subtract(b);
 
 			// Error
 			RealMatrix error = x_hut.subtract(x);
 
+			int numberOfOnes = equalVectorsToOne(x_hut.getData());
+
 			// Infinity norm
-			double residdumInfNorm = b.getNorm() - H.getNorm();
-			double errorInfNorm = error.getNorm();
+			// double residdumInfNorm = residuum.getNorm();
+			// double errorInfNorm = error.getNorm();
 
-			System.out.println(n + "\t\t\t\t" + residdumInfNorm + "\t\t\t\t" + errorInfNorm);
+			// for example use the b-vector inf norm
+			Matrix jamaMatrixResiduum = new Matrix(residuum.getData());
+			Matrix jamaMatrixError = new Matrix(error.getData());
 
+			System.out.println(n + "\t" + jamaMatrixResiduum.normInf() + "\t\t\t\t\t\t" + jamaMatrixError.normInf()
+					+ "\t\t\t\t\t\t\t" + conitionNumber + "\t\t\t\t\t\t\t" + numberOfOnes);
 		}
+	}
+
+	private static int equalVectorsToOne(double[][] vector) {
+		int counter = 0;
+
+		// GJCore core = new GJCore();
+		// core.printMatrix(vector);
+
+		for (int i = 0; i < vector.length; i++) {
+			if (vector[i][0] == 1.d) {
+				counter = counter++;
+			}
+		}
+
+		return counter;
 	}
 }
